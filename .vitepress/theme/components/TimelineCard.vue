f7bdc187ec32c350b8c2f57313432b457a721715
Gianlorenzo Occhipinti <gianlorenzo.occhipinti@gmail.com>
2024-05-24 18:23:57 +0200

    refactoring of TimelineCard

--- .vitepress/theme/components/TimelineCard.vue
+++ .vitepress/theme/components/TimelineCard.vue
@@ -1,24 +1,22 @@
 <script setup>
-import {data as projects} from "../loader/projects.data"
+import { data as projects } from "../loader/projects.data"
 </script>
 <template>
-    <a :href="project.url" class="timeline-project-v2" v-if="project.found">
-        <div class="timeline-project-v2-image">
+    <a :href="project.id" class="timeline-project" v-if="project.found">
+        <div class="timeline-project-image">
             <img :src="project.image" />
         </div>
 
-        <div class="timeline-project-v2-content">
-            <div class="timeline-project-v2-content-title">
-                <h2 :id="project.id" tabindex="-1"> {{ project.title }}
-                    <a class="header-anchor" :href='headerAnchor' aria-label="Permalink">​</a>
-                </h2>
+        <div class="timeline-project-content">
+            <div class="timeline-project-content-title">
+                <h2 :id="project.id" tabindex="-1"> {{ project.title }} </h2>
             </div>
 
-            <div class="timeline-project-v2-content-description">
+            <div class="timeline-project-content-description">
                 {{ project.description }}
             </div>
 
-            <div class="timeline-project-v2-content-badges">
+            <div class="timeline-project-content-badges">
                 <Badge v-for="tag in project.tags" :key="tag">{{ tag }}</Badge>
             </div>
         </div>
@@ -39,8 +37,8 @@ export default {
     },
     computed: {
         project() {
-            const project = projects.find(project => project.frontmatter.projectId ?? undefined === this.projectId)
-            return {found: project !== undefined, ...project.frontmatter}
+            const project = projects.find(project => project.frontmatter.id ?? undefined === this.projectId)
+            return { found: project !== undefined, ...project?.frontmatter }
         },
         headerAnchor() {
             return '#' + this.project.id;
@@ -50,22 +48,21 @@ export default {
 </script>
 
 <style>
-.timeline-project-v2-content-badges {
+.timeline-project-content-badges {
     margin-top: 10px;
 }
 
-.timeline-project-v2:hover {
-    background-color: #f6f6f7;
+.timeline-project:hover {
     border-radius: 20px;
     cursor: pointer;
     color: var(--homepage-contrast-color);
 }
 
-.timeline-project-v2:hover {
+.timeline-project:hover {
     border: 5px solid var(--vp-c-text-1);
 }
 
-.timeline-project-v2 {
+.timeline-project {
     display: flex;
 
     flex-direction: row;
@@ -83,26 +80,22 @@ export default {
     align-items: stretch;
 }
 
-.timeline-project-v2>h2 {
-    display: none
-}
-
-.timeline-project-v2-image {
+.timeline-project-image {
     max-width: 15vw;
 }
 
-.timeline-project-v2-image>img {
+.timeline-project-image>img {
     border-radius: 20px;
 }
 
-.timeline-project-v2-content {
+.timeline-project-content {
     display: flex;
     justify-content: space-between;
     flex-direction: column;
     color: var(--vp-c-text-1)
 }
 
-.timeline-project-v2-content-title {
+.timeline-project-content-title {
     font-weight: 600;
     font-size: 2rem;
     margin-bottom: 20px;
@@ -110,34 +103,33 @@ export default {
     padding-top: 20px;
 }
 
-.timeline-project-v2-content-title > h2 {
+.timeline-project-content-title > h2 {
     margin: 0;
     border: 0;
     padding-top: 0;
 }
 
 @media screen and (max-width: 600px) {
-    .timeline-project-v2 {
+    .timeline-project {
         flex-direction: column;
         align-items: center;
     }
 
-    .timeline-project-v2-image {
+    .timeline-project-image {
         max-width: 100%;
     }
 
-    .timeline-project-v2-content {
+    .timeline-project-content {
         width: 100%;
     }
-    
-    .timeline-project-v2-content-title {
+
+    .timeline-project-content-title {
         font-size: 1.5rem;
         text-align: center;
     }
 
-    .timeline-project-v2-content-title {
+    .timeline-project-content-title {
         padding-top: 0;
     }
 }
-
 </style>
--- project/event-manager.md
+++ project/event-manager.md
@@ -4,8 +4,7 @@ description: This is the first project.
 tags: [tag1, tag2]
 image: /img/placeholder.svg
 techs: [Vue, Vite, VitePress]
-projectId: 'event-manager'
-url: 'project/event-manager'
+id: 'event-manager' # must concide with file name
 ---
 
 This is the detailed description of Project One.
